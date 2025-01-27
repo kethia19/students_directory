aaa255dcd20c8576ee3dee692c334336e8773d2f
frank00067 <f.nkurunziz2@alustudent.com>
2025-01-27 03:47:09 -0800

    student name cohort-1

--- the_coder.sh
+++ the_coder.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#The following line is for creating a directory called "students_directory"
-mkdir students_directory
+#In this directory, we are adding three text files using the following command
 
-#In the directory above, we are adding three text files using the following command
-
-touch students_directory/cohort-1.txt
-touch students_directory/cohort-2.txt
-touch students_directory/cohort-3.txt
+touch cohort-1.txt
+touch cohort-2.txt
+touch cohort-3.txt
+# Adding text (students names) into cohort-1.txt
+echo -e "faustin1 \nfredy2 \nfabrice3"> cohort-1.txt
 
