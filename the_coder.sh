61bfaef183448bf5a4f6be3f45607ac8360da0d4
kethia19 <k.kayigire@alustudent.com>
2025-01-27 11:04:10 +0200

    Hey, I added the_coder.sh file in the repository

---
+++ the_coder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#The following line is for creating a directory called "students_directory"
+mkdir students_directory
+
+#In the directory above, we are adding three text files using the following command
+
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
+
